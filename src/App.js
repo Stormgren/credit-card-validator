5d89c0c5f2875f4ae759ba85ead8169826e44499
Slobodan Vukovic <sbnv93@gmail.com>
2020-02-16 04:57:22 +0100

    Initial commit, started work on updating the state from input

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  
+  constructor(){
+    super();
+    this.state = {
+      cardNumbers: '',
+      numbers: []
+    }
+    this.changeHandler = this.changeHandler.bind(this);
+  }
+
+  changeHandler = (e) => {
+    this.setState({
+      cardNumbers: e.target.value
+    });
+    console.log(this.state.cardNumbers, this.state.numbers)
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault();
+    let arr = this.state.cardNumbers
+    this.setState({
+      numbers: arr.split('')
+    })
+    console.log(this.state.cardNumbers, this.state.numbers)
+  }
+
+  render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h3>Card Validator</h3>
+      <div className="form-input">
+        <input type="text" value={this.state.cardNumbers} onChange={this.changeHandler}/>
+        <input type="submit" value="submit" onClick={this.submitHandler}/>
+
+        <h3>{this.state.cardNumbers}</h3>
+
+  
+      
+        </div>
       </header>
     </div>
   );
+}
 }
 
 export default App;
